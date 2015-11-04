4b745ef9a6770cca0d23fef52165433fd97a722a
Fernando Verdaguer <fc.verdaguer@gmail.com>
2015-11-04 08:24:13 -0300

    pruebas
    fb login, partial view, etc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
+var entities = require('../data/models/entities');
 var express = require('express');
 var router = express.Router();
 
+var provincia = entities.Provincia;
+
+var provincias = provincia.findAll().then(function(provincias){
+    provincias = provincias.map(function(provincia){ return provincia.toJSON() });
+    return provincias;
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', provincias: provincias });
 });
+
+/* GET Fb page. */
+router.get('/facebook', function(req, res) {
+   res.render('facebook', { title: 'Facebook Login' });
+});
+
+console.log(provincias);
+
+
 
 module.exports = router;
